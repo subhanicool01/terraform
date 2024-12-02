259e386f0c3b519817a8f01890d104a19f2158b1
Mehaboob Shaik <mshaik@procurant.com>
2024-12-02 10:22:41 +0530

    added k8s groovy

---
+++ settings/Project/4-SharedLibrary/src/.com/builds/k8s.groovy
@@ -0,0 +1,9 @@
+def k8sdeploy(env_Name.yaml, docker_image) {
+    jenkins.sh """#!bin/bash
+    echo "executing k8sdeploy method"
+    echo " final image name is $docker_image"
+    // DIT or yaml file image name should be not same, so replaced with current image
+    sed -i "s|DIT|$docker_image" ./.cicd/$env_Name.yaml
+    kubectl apply -f ./.cicd/$env_Name.yaml
+    """
+}
